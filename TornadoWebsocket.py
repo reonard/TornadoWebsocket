0a863e40e506f7b7f4441a456a61bffdff295ec3
reonard <reonard@126.com>
2015-09-15 10:02:52 +0800

    update json message framework

--- TornadoWebsocket.py
+++ TornadoWebsocket.py
@@ -5,9 +5,16 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
+import tornado.gen
 import time
+import json
+import tornado.httpclient
+
+tornado.httpclient.AsyncHTTPClient.fetch()
+
 
 connectedClient = {}
+
 
 class Application(tornado.web.Application):
 
@@ -26,11 +33,16 @@ class MonitorHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         time.sleep(1)
-        self.tid = message[2:]
+        decoded_msg = json.load()
+
+        self.tid = json.load(message)['']
         if self.tid not in connectedClient.keys():
             connectedClient[self.tid] = self
         print ("%s is beating" % self.tid)
-        self.write_message("Received", 0)
+
+        decoded = json.load(message)
+        self.process_message(decoded)
+
         print message
 
     def on_close(self):
@@ -38,9 +50,30 @@ class MonitorHandler(tornado.websocket.WebSocketHandler):
             del connectedClient[self.tid]
             print "close"
 
+    def process_message(self, json_message):
+        action_type = json_message.get('type')
+        if action_type == 'HB':
+            self.__heartbeatRslt__(json_message.get('result'))
+        elif action_type == 'InqResp':
+            self.__inquiryRslt__(json_message.get('result'))
+        elif action_type == 'UpdResp':
+            self.__updateRslt__(json_message.get('result'))
+
+    def __heartbeatRslt__(self, result_msg):
+        print result_msg
+        resp = json.dumps({'type': 'HB', 'command': 'Received'})
+        self.write_message(resp, 0)
+
+    def __inquiryRslt__(self, result_msg):
+        print result_msg
+
+    def __updateRslt(self, result_msg):
+        print result_msg
+
 
 class ManagerHandler(tornado.web.RequestHandler):
-
+    @tornado.web.asynchronous
+    @tornado.gen.engine
     def get(self):
         command = self.get_argument('command', ' ')
         target = self.get_argument('target', ' ')
@@ -50,7 +83,14 @@ class ManagerHandler(tornado.web.RequestHandler):
 
         if target in connectedClient.keys():
             print "target found, sending"
-            connectedClient[target].write_message(command)
+            json_cmd = json.dumps({'type': 'INQ', 'command': command})
+            connectedClient[target].write_message(json_cmd)
+
+        yield tornado.gen.Task(self.wait, {})
+
+    def wait(self, args, callback):
+        time.sleep(5)
+        return
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,30 +2,54 @@ import websocket
 import thread
 import time
 import re
+import json
 import MySQLdb
 
 COnn = 0
 
-def heartbeatRes():
-    pass
 
-def inquiryRes(ws, inq):
-    #cursor = db.cursor
-    ws.send(result)
+def heartbeatRes(wst, msg):
+    print "HeartBeating"
+    print msg
+
+
+def inquiryRes(wst, msg):
+    print "Inquiring"
+    print msg
+    jsonResp = json.dumps({'type': 'InqResp', 'result': 'Received %s' % msg})
+    wst.send(jsonResp)
+
+
+def updateRes(wst, msg):
+    print "Updating"
+    print msg
+
+actionList = {'HB': heartbeatRes,
+              'INQ': inquiryRes,
+              'UPD': updateRes}
+
+
+def process_message(wst, json_message):
+    action_type = json_message.get('type')
+    if action_type in actionList.keys():
+        actionList.get(action_type)(ws, json_message.get('command'))
+
 
 def on_message(ws, message):
     print message
-    rsl = re.search(r'(.*)-:-(.*)', message)
-    if rsl:
-        rsl.group(1)
+    decoded = json.loads(message)
+    process_message(ws, decoded)
+
 
 def on_error(ws, error):
     print error
+
 
 def on_close(ws):
     global COnn
     print "### closed ###"
     COnn = 0
+
 
 def on_open(ws):
     print "open"
@@ -36,7 +60,7 @@ def on_open(ws):
         while True:
             time.sleep(5)
             print "sending"
-            ws.send("HB2")
+            ws.send("HB1")
         print "thread terminating..."
     thread.start_new_thread(run, ())
 
